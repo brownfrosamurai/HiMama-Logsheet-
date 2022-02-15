c188959221d75f7a0deb6996c0a985cb41f69c1b
meduna femi <femimeduna@gmail.com>
2022-02-15 11:10:23 -0700

    heroku deploy

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(req, res, next) {
         const reqType = req.headers["x-forwarded-proto"];
         // if not https redirect to https unless logging in using OAuth
         if (reqType !== "https") {
-            req.url.indexOf("auth/google") !== -1
+            req.url.indexOf("auth/google/callback") !== -1
               ? next()
               : res.redirect("https://" + req.headers.host + req.url);
         } 
