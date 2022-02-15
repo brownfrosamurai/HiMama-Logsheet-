cb72f93310c9e5b6d53bae6c11c2c02df93af4f0
meduna femi <femimeduna@gmail.com>
2022-02-15 10:30:13 -0700

    heroku deploy

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,6 @@ module.exports = function (passport) {
         callbackURL: '/auth/google/callback',
     },
         async (accessToken, refreshToken, profile, done) => {
-            console.log(profile)
             const newUser = {
                 googleId: profile.id,
                 displayName: profile.displayName,
