943dd51a0031926df03893d5bd4f7524f02db112
meduna femi <femimeduna@gmail.com>
2022-02-18 11:17:30 -0700

    heroku deploy

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://himama-logsheet.herokuapp.com/auth/google/callback',
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
         proxy: true
     },
         async (accessToken, refreshToken, profile, done) => {
